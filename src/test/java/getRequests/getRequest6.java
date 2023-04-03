4c5d8bcf16a13999950a98e76e70c385398a23dc
Ercan ALTUN <salsel0147@gmail.com>
2023-04-03 12:16:59 +0300

    03.04.2023

--- src/test/java/getRequests/getRequest6.java
+++ src/test/java/getRequests/getRequest6.java
@@ -11,7 +11,7 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class getRequests6 extends JsonplaceholderBaseUrl {
+public class getRequest6 extends JsonplaceholderBaseUrl {
 
 
     /*
